a4c4e38ef260a001876ba360befdf604eeb8cbec
Nick DePalma <ndepalma@gmail.com>
2023-09-20 02:07:59 -0400

    Cleaning up the install

--- Formula/functional-dag.rb
+++ Formula/functional-dag.rb
@@ -4,7 +4,7 @@ class FunctionalDag < Formula
   desc "A library for building functional dags"
   homepage "https://github.com/petrogly-ph/functional-dag"
   url "https://github.com/petrogly-ph/functional-dag/archive/refs/tags/brew.tar.gz"
-  sha256 "6f19bce91912b14324aa469855f8516bce9e797b014ce0a7ada3f79a2ca90627"
+  sha256 "38775c8234d598e1e1f099861a19e000564a08c66ff15643bbf6cc8e5d707ed1"
   license "MIT"
   version "0.5"
 
@@ -27,6 +27,7 @@ class FunctionalDag < Formula
     end
   end
 
+  # Testing is performed manually until v1.0
   # test do
   #   system "false"
   # end
