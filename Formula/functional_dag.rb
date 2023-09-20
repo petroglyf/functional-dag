ccfc72cc4edf1948a878ba7fbb775c549ff797a7
Nick DePalma <ndepalma@gmail.com>
2023-09-20 01:18:34 -0400

    Make install doesn't install to right directory. fixing that

--- Formula/functional_dag.rb
+++ Formula/functional_dag.rb
@@ -1,18 +1,17 @@
 require "formula"
 
-class Functionaldag < Formula
+class FunctionalDag < Formula
   desc "A library for building functional dags"
   homepage "https://github.com/petrogly-ph/functional-dag"
   url "https://github.com/petrogly-ph/functional-dag/archive/refs/tags/brew.tar.gz"
-  sha256 "74f8d3301e713c47c265899316c92627281d923d960bb3a1aa7f4f4c73542682"
+  sha256 "d649bbc13cbb81fca2d686a17be9609357f702212ed201076010425ed72e62b2"
   license "MIT"
-  version "1.0"
+  version "0.5"
 
+  # Don't yet support bottles! 
   # bottle do
   #   cellar :any
-  #   sha1 "5466fbee57b366a41bbcec814614ee236e39bed8" => :yosemite
-  #   sha1 "bde270764522e4a1d99767ca759574a99485e5ac" => :mavericks
-  #   sha1 "e77d0e5f516cb41ac061e1050c8f37d0fb65b796" => :mountain_lion
+  #   sha1 "5466fbee57b366a41bbcec814614ee236e39bed8" => :ventura
   # end
 
   depends_on "cmake" => :build
@@ -20,19 +19,12 @@ class Functionaldag < Formula
   depends_on "jsoncpp" => :build
 
   def install
-    # ENV.cxx20 if build.cxx20?
     mkdir "filterdag-build" do
       args = std_cmake_args
       
       system "cmake", "..", *args
       system "make", "install"
     end
-
-
-    # ENV.deparallelize
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    # system "make", "install"
   end
 
   # test do
