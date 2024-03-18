fd2fffe694d642f3e97dd4b11c0f23c5ab04d74c
Nick DePalma <ndepalma@gmail.com>
2024-03-18 00:14:57 -0400

    Strong thing to try.. not sure if this will work

--- cmake-toolchains/arm64-tc.cmake
+++ cmake-toolchains/arm64-tc.cmake
@@ -7,6 +7,7 @@ set(CMAKE_CXX_COMPILER g++)
 
 # where is the target environment located
 set(CMAKE_FIND_ROOT_PATH /usr/lib/aarch64-linux-gnu)
+set(CMAKE_PREFIX_PATH /usr/lib/aarch64-linux-gnu)
 message("Setting find root path to aarch64")
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
