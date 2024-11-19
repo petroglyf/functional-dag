067b6590787472cdd8eb8a78b03a8c4549a3eb24
ND <ndepalma@gmail.com>
2024-11-18 22:28:27 -0500

    Moving to the meson build system and creating a simple flatbuffer example (#28)

--- Formula/functional-dag.rb
+++ Formula/functional-dag.rb
@@ -1,12 +1,10 @@
-require "formula"
-
 class FunctionalDag < Formula
-  desc "A library for building functional dags"
+  desc "This library is used for building functional dags."
   homepage "https://github.com/petrogly-ph/functional-dag"
   url "https://github.com/petrogly-ph/functional-dag/archive/refs/tags/brew-v0.5.tar.gz"
-  sha256 "284fa1af920934c4660bbc7954e6854bf485c20f5ba85f8b5f73d0a1a7387f72"
   license "MIT"
   version "0.5"
+  #sha256 "284fa1af92093a4c4660bbc7954e6854bf485c20f5ba85f8b5f73d0a1a7387f72"
 
   # Don't yet support bottles! 
   # bottle do
@@ -14,21 +12,22 @@ class FunctionalDag < Formula
   #   sha1 "5466fbee57b366a41bbcec814614ee236e39bed8" => :ventura
   # end
 
-  depends_on "cmake" => :build
-  depends_on "catch2" => :test
+  depends_on "meson" => :build
   depends_on "jsoncpp" => :build
+  depends_on "catch2" => :test
 
   def install
-    mkdir "filterdag-build" do
-      args = std_cmake_args
-      
-      system "cmake", "..", *args
-      system "make", "install"
+    build_directory = "filterdag-build"
+    args = std_meson_args
+    
+    mkdir build_directory do
+      system "meson", "setup", *args, ".."      
+      system "meson", "install"
     end
   end
 
   # Testing is performed manually until v1.0
-  # test do
-  #   system "false"
-  # end
+  test do
+    system "meson", "test"
+  end
 end
