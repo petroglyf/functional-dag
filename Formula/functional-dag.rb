d1d42e806febfd7b6ef41ded8424e887b74b74f0
ND <ndepalma@gmail.com>
2025-01-05 23:03:36 -0500

    Fixing the homebrw formula (#34)

--- Formula/functional-dag.rb
+++ Formula/functional-dag.rb
@@ -1,32 +1,34 @@
 class FunctionalDag < Formula
   desc "This library is used for building functional dags"
   homepage "https://github.com/petrogly-ph/functional-dag"
-  url "https://github.com/petrogly-ph/functional-dag/archive/refs/tags/brew-v1.0rc1.tar.gz"
+  url "https://github.com/petroglyf/functional-dag/archive/refs/tags/brew-v1.0rc1.tar.gz"
   version "1.0rc1"
   license "MIT"
-  sha256 "284fa1af92093a4c4660bbc7954e6854bf485c20f5ba85f8b5f73d0a1a7387f72"
-
-  # Don't yet support bottles!
-  bottle do
-    cellar :any
-    sha1 "5466fbee57b366a41bbcec814614ee236e39bed8" => :sequoia
-  end
+  sha256 "24a7db0ae53871cfe9c9f35e81077a419f6d8880d7485ec2a924a81519e74653"
 
   depends_on "meson" => :build
   depends_on "catch2" => :test
+  depends_on "ninja" => :build
+  depends_on "flatbuffers" => :build
 
   def install
-    build_directory = "filterdag-build"
-    args = std_meson_args
-    
-    mkdir build_directory do
-      system "meson", "setup", *args, ".."
-      system "meson", "install"
-    end
+    system "meson", "setup", "build", *std_meson_args
+    system "ninja", "-C", "build", "libfunctional_dag.dylib"
+    system "meson", "install", "-C", "build"
   end
 
   # Testing is performed manually until v1.0
   test do
-    system "meson", "test"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test functional-dag`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    # system "meson", "test"
+    system "false"
   end
 end
