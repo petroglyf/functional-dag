f2aaa86c2d5cac7299c21841caacf4c3e86e2385
Nick DePalma <ndepalma@gmail.com>
2025-01-05 22:30:33 -0500

    YY

--- Formula/functional-dag.rb
+++ Formula/functional-dag.rb
@@ -4,7 +4,7 @@ class FunctionalDag < Formula
   url "https://github.com/petroglyf/functional-dag/archive/refs/tags/brew-v1.0.tar.gz"
   version "1.0rc1"
   license "MIT"
-  sha256 "249e5b7790f8d47a817b44123c3a4513a49711222ffbce6e6c1ba8e14772ec2e"
+  sha256 "6fe48b8dd403de474bd10ea7403dd2a028c711e0a1a671b764a421693d1be66a"
 
   # Don't yet support bottles!
   # bottle do
